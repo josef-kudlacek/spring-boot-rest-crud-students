ff2ea1b7600ee16a9f69e5d2e57d753e805ed465
Josef Kudláček <j.k.kudlacek@gmail.com>
2024-08-02 16:02:48 +0200

    Code refactor

--- src/main/java/eu/kudljo/demo/rest/StudentRestController.java
+++ src/main/java/eu/kudljo/demo/rest/StudentRestController.java
@@ -1,6 +1,7 @@
 package eu.kudljo.demo.rest;
 
 import eu.kudljo.demo.entity.Student;
+import jakarta.annotation.PostConstruct;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,14 +13,19 @@ import java.util.List;
 @RequestMapping("/api")
 public class StudentRestController {
 
-    @GetMapping("/students")
-    public List<Student> getStudent() {
-        List<Student> students = new ArrayList<>();
+    private ArrayList<Student> students;
+
+    @PostConstruct
+    public void loadData() {
+        students = new ArrayList<>();
 
         students.add(new Student("Poornima", "Patel"));
         students.add(new Student("Mario", "Rossi"));
         students.add(new Student("Mary", "Smith"));
+    }
 
+    @GetMapping("/students")
+    public List<Student> getStudent() {
         return students;
     }
 }
